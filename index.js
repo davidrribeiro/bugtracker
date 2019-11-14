8c4a855563077897a9281792110a6e7f0a799fe1
talkdsign <talkdsign@gmail.com>
2019-11-13 22:08:25 -0300

    integração com sendgrid+criação de promises

--- index.js
+++ index.js
@@ -2,41 +2,64 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
+const { promisify } = require('util')
+const sgMail = require('@sendgrid/mail')
 
 const GoogleSpreadsheet = require('google-spreadsheet')
 const credentials = require('./bugtracker.json')
 
 //Configs
-const docId='1LUkJreRxfFQBUeXFgc1kV7OZNgpP5Z4v_H8kpqXqKYk'
+const docId = '1LUkJreRxfFQBUeXFgc1kV7OZNgpP5Z4v_H8kpqXqKYk'
 const worksheetIndex = 0
+const sendgridkey = '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'
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, 'views'))
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (request, response) => {
     response.render('home')
 })
 
-app.post('/', (request, response)=> {
-    const doc = new GoogleSpreadsheet(docId)
-    doc.useServiceAccountAuth(credentials, (err) =>{
-    if (err) {
-        console.log('Não foi possível abrir a planilha')
-    } else {
+app.post('/', async (request, response) => {
+    try {
+        const doc = new GoogleSpreadsheet(docId)
+        await promisify(doc.useServiceAccountAuth)(credentials)
         console.log('Planilha aberta')
-        doc.getInfo((err, info) => {
-            const worksheet = info.worksheets[worksheetIndex]
-            worksheet.addRow({name: request.body.name, email: request.body.email}, err =>{
-                response.send('bug reportado com sucesso')
-            })
-
+        const info = await promisify(doc.getInfo)()
+        const worksheet = info.worksheets[worksheetIndex]
+        await promisify(worksheet.addRow)({
+            name: request.body.name,
+            email: request.body.email,
+            issueType: request.body.issueType,
+            source: request.query.source || 'direct',
+            userAgent: request.body.userAgent,
+            userDate: request.body.userDate
         })
 
-    }
-})  
+        // Se for crítico
 
+        if (request.body.issueType === 'CRITICAL') {
+            sgMail.setApiKey(sendgridkey)
+            const msg = {
+                to: 'talkdsign@gmail.com',
+                from: 'talkdsign@gmail.com',
+                subject: 'Erro Crítico reportado',
+                text: `
+                O usuário ${request.body.name} reportou um problema`,
+                html: `O usuário <strong>${request.body.name}</strong> reportou um problema`,
+            }
+            await sgMail.send(msg)
+
+        }
+
+
+        response.send('Bug reportado com sucesso')
+    } catch (err) {
+        response.send('Erro ao enviar o formulário.')
+        console.log(err)
+    }
 })
 
 
--- testar-planilha.js
+++ testar-planilha.js
@@ -1,5 +1,19 @@
 const GoogleSpreadsheet = require('google-spreadsheet')
 const credentials = require('./bugtracker.json')
+const { promisify } = require('util')
+
+const addRowToSheet = async() => {
+    const doc = new GoogleSpreadsheet('1LUkJreRxfFQBUeXFgc1kV7OZNgpP5Z4v_H8kpqXqKYk')
+    await promisify(doc.useServiceAccountAuth) (credentials)
+    console.log('Planilha aberta')
+    const info = await promisify(doc.getInfo) ()
+    const worksheet = info.worksheets[0]
+    await promisify(worksheet.addRow)({name: 'David Ribeiro', email: 'test@test.com'})
+}
+
+addRowToSheet()
+
+/*
 
 const doc = new GoogleSpreadsheet('1LUkJreRxfFQBUeXFgc1kV7OZNgpP5Z4v_H8kpqXqKYk')
 doc.useServiceAccountAuth(credentials, (err) =>{
@@ -17,3 +31,5 @@ doc.useServiceAccountAuth(credentials, (err) =>{
 
     }
 })
+
+*/
